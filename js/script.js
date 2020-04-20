6a5af65a89ac4abdc5d472e393aa051647168b70
Daniel Amariles <daniel.amariles@metromile.com>
2020-04-20 10:11:32 -0500

    añadiendo pico y placa anterior

--- js/script.js
+++ js/script.js
@@ -27,36 +27,42 @@ $("input[type='radio']").click(function () {
 function updateResult() {
     var cedula = $("#cedula-menu input[type='radio']:checked").val();
     var placa = $("#placa-menu input[type='radio']:checked").val();
-    var days=['LUNES','MARTES','MIÉRCOLES','JUEVES','VIERNES','SÁBADO', 'DOMINGO']
+    var days = ['LUNES', 'MARTES', 'MIÉRCOLES', 'JUEVES', 'VIERNES', 'SÁBADO', 'DOMINGO']
 
-    pico_cedula =  [['1','2'],['3','4'],['5','6'],['7','8'],['9','0'],['1','3','5','7','9'], ['0','2','4','6','8']]
-    pico_placa =  [['1','3','5','7','9'],['0','2','4','6','8'],['1','3','5','7','9'],['0','2','4','6','8'],['1','3','5','7','9'],['0','2','4','6','8'], ['1','3','5','7','9','0','2','4','6','8']]
-
+    pico_cedula = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '8'], ['9', '0'], ['1', '3', '5', '7', '9'], ['0', '2', '4', '6', '8']]
+    pico_vida = [['1', '3', '5', '7', '9'], ['0', '2', '4', '6', '8'], ['1', '3', '5', '7', '9'], ['0', '2', '4', '6', '8'], ['1', '3', '5', '7', '9'], ['0', '2', '4', '6', '8'], ['1', '3', '5', '7', '9', '0', '2', '4', '6', '8']]
+    pico_placa = [['5', '6'], ['7', '8'], ['9', '0'], ['1', '2'], ['3', '4'], [], []]
     console.log(cedula + " " + placa)
 
-    for(i=0;i<9;i++){
-        if (cedula != undefined){
-            if(pico_cedula[i].includes(cedula)){
-                if (placa != undefined){
-                    if(pico_placa[i].includes(placa)){
-                        $(".day #day-"+i).prop("class","box_yes");
-                        $(".day #day-"+i).html('<span><i class="fas fa-walking"> </i> '+days[i]+': Puedes salir, sin vehículo <i class="fas fa-ban"><i class="fas fa-car"> </span>');
-                    }else{
-                        $(".day #day-"+i).prop("class","box_yes");
-                        $(".day #day-"+i).html('<span><i class="fas fa-car"> </i> '+days[i]+': Puedes salir, con vehículo <i class="fas fa-check"><i class="fas fa-car"></span>');
+    for (i = 0; i < 9; i++) {
+        if (cedula != undefined) {
+            if (pico_cedula[i].includes(cedula)) {
+                if (placa != undefined) {
+                    if (pico_vida[i].includes(placa)) {
+                        $(".day #day-" + i).prop("class", "box_yes");
+                        $(".day #day-" + i).html('<span><i class="fas fa-walking"> </i> ' + days[i] + ': Puedes salir, sin vehículo <i class="fas fa-ban"><i class="fas fa-car"> </span>');
+                    } else {
+                        if (pico_placa[i].includes(placa)) {
+                            $(".day #day-" + i).prop("class", "box_yes");
+                            $(".day #day-" + i).html('<span><i class="fas fa-clock"> </i> ' + days[i] + ': Puedes salir, con vehículo en las franjas permitidas <i class="fas fa-clock"><i class="fas fa-car"></span>');
+                        }else{
+                            $(".day #day-" + i).prop("class", "box_yes");
+                            $(".day #day-" + i).html('<span><i class="fas fa-car"> </i> ' + days[i] + ': Puedes salir, con vehículo 24 horas <i class="fas fa-check"><i class="fas fa-car"></span>');
+                        
+                        }
                     }
-                }else{
-                    $(".day #day-"+i).prop("class","box_yes");
-                    $(".day #day-"+i).html('<span><i class="fas fa-walking"> </i> '+days[i]+': Puedes salir');
+                } else {
+                    $(".day #day-" + i).prop("class", "box_yes");
+                    $(".day #day-" + i).html('<span><i class="fas fa-walking"> </i> ' + days[i] + ': Puedes salir');
                 }
 
-            }else{
-                $(".day #day-"+i).prop("class","box_no");
-                $(".day #day-"+i).html('<span><i class="fas fa-ban"> </i> '+days[i]+': Quédate en casa</span>');
+            } else {
+                $(".day #day-" + i).prop("class", "box_no");
+                $(".day #day-" + i).html('<span><i class="fas fa-ban"> </i> ' + days[i] + ': Quédate en casa</span>');
             }
-        }else{
-            $(".day #day-"+i).prop("class","box_no");
-            $(".day #day-"+i).html('<span><i class="fas fa-ban"> </i> '+days[i]+': Quédate en casa</span>');
+        } else {
+            $(".day #day-" + i).prop("class", "box_no");
+            $(".day #day-" + i).html('<span><i class="fas fa-ban"> </i> ' + days[i] + ': Quédate en casa</span>');
 
         }
 
