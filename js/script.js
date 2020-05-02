53d95a4961697dc9cb146f96db405190bc7d4391
Daniel Amariles <daniel.amariles@metromile.com>
2020-05-02 16:18:35 -0500

    mayo 4 al 10

--- js/script.js
+++ js/script.js
@@ -27,34 +27,29 @@ $("input[type='radio']").click(function () {
 function updateResult() {
     var cedula = $("#cedula-menu input[type='radio']:checked").val();
     var placa = $("#placa-menu input[type='radio']:checked").val();
-    var days = ['LUNES', 'MARTES', 'MIÉRCOLES', 'JUEVES', 'VIERNES', 'SÁBADO', 'DOMINGO']
+    var vehicle = $("#vehicle-menu input[type='radio']:checked").val();
+    var days = ['LUNES 4', 'MARTES 5', 'MIÉRCOLES 6', 'JUEVES 7', 'VIERNES 8', 'SÁBADO 9', 'DOMINGO 10']
 
     pico_cedula = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '8'], ['9', '0'], ['1', '3', '5', '7', '9'], ['0', '2', '4', '6', '8']]
-    pico_vida = [['1', '3', '5', '7', '9'], ['0', '2', '4', '6', '8'], ['1', '3', '5', '7', '9'], ['0', '2', '4', '6', '8'], ['1', '3', '5', '7', '9'], ['0', '2', '4', '6', '8'], ['1', '3', '5', '7', '9', '0', '2', '4', '6', '8']]
-    pico_placa = [['5', '6'], ['7', '8'], ['9', '0'], ['1', '2'], ['3', '4'], [], []]
-    console.log(cedula + " " + placa)
+    pico_placa_particular = [['5', '6'], ['7', '8'], ['9', '0'], ['1', '2'], ['3', '4'], [], []]
 
     for (i = 0; i < 7; i++) {
         if (cedula != undefined) {
             if (pico_cedula[i].includes(cedula)) {
-                if (placa != undefined) {
-                    if (pico_vida[i].includes(placa)) {
-                        $(".day #day-" + i).prop("class", "box_yes");
-                        $(".day #day-" + i).html('<span><i class="fas fa-walking"> </i> ' + days[i] + ': Puedes salir, sin vehículo <i class="fas fa-ban"><i class="fas fa-car"> </span>');
-                    } else {
-                        if (pico_placa[i].includes(placa)) {
-                            $(".day #day-" + i).prop("class", "box_yes");
-                            $(".day #day-" + i).html('<span><i class="fas fa-clock"> </i> ' + days[i] + ': Puedes salir, con vehículo en horas permitidas <i class="fas fa-clock"><i class="fas fa-car"></span>');
-                        }else{
-                            $(".day #day-" + i).prop("class", "box_yes");
-                            $(".day #day-" + i).html('<span><i class="fas fa-car"> </i> ' + days[i] + ': Puedes salir, con vehículo 24 horas <i class="fas fa-check"><i class="fas fa-car"></span>');
-                        
-                        }
-                    }
-                } else {
-                    $(".day #day-" + i).prop("class", "box_yes");
-                    $(".day #day-" + i).html('<span><i class="fas fa-walking"> </i> ' + days[i] + ': Puedes salir');
-                }
+
+                            if (placa != undefined) {
+                                if (pico_placa_particular[i].includes(placa)) {
+                                    $(".day #day-" + i).prop("class", "box_yes");
+                                    $(".day #day-" + i).html('<span><i class="fas fa-clock"> </i> ' + days[i] + ': Tu vehículo puede salir en horas permitidas <i class="fas fa-clock"><i class="fas fa-car"></span>');
+                                } else {
+                                    $(".day #day-" + i).prop("class", "box_yes");
+                                    $(".day #day-" + i).html('<span><i class="fas fa-car"> </i> ' + days[i] + ': Tu vehículo puede salir 24 horas <i class="fas fa-check"><i class="fas fa-car"></span>');
+
+                                }
+                            } else {
+                                $(".day #day-" + i).prop("class", "box_yes");
+                                $(".day #day-" + i).html('<span><i class="fas fa-walking"> </i> ' + days[i] + ': Puedes salir caminando');
+                            }
 
             } else {
                 $(".day #day-" + i).prop("class", "box_no");
